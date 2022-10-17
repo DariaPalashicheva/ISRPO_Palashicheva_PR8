5dac3530f1ae769d4c80d662b6b52cb6c410d628
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-10-17 14:50:41 +1000

    вариант11

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -10,22 +10,21 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ISRPO_Palashicheva_PR8
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
         }
 
-        private void Schet_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             // Считывание начальных данных
 
@@ -33,26 +32,20 @@ namespace ISRPO_Palashicheva_PR8
             double xk = Convert.ToDouble(Xk.Text);
             double dx = Convert.ToDouble(Dx.Text);
             double a = Convert.ToDouble(A.Text);
+            double b = Convert.ToDouble(B.Text);
 
             // Цикл для табулирования функции
 
             double x = x0;
 
-            while (x<= (xk+dx/2))
+            while (x <= (xk + dx / 2))
             {
-                double y = a * Math.Log(x);
+                double y = x + (Math.Sqrt(Math.Abs(Math.Pow(x,3)+a-b*(Math.Exp(x)))));
 
                 X.Text += "x=" + Convert.ToString(x) + Environment.NewLine;
-                У.Text += "y=" + Convert.ToString(y) + Environment.NewLine;
+                Y.Text += "y=" + Convert.ToString(y) + Environment.NewLine;
                 x = x + dx;
             }
-
-        }
-        private void Individ_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 a = new Window1();
-            a.Show();
-            main.Close();
 
         }
     }
